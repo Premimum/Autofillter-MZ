0088ef608ea24e10e2244e9a56810c6ad934b2e5
Premimum <129716070+Premimum@users.noreply.github.com>
2023-04-21 12:53:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Premimum/Autofillter-MZ.git /Autofillter-MZ
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Autofillter-MZ
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Autofillter-MZ...."
 python3 bot.py
